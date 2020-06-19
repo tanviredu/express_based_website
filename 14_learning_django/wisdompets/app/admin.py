712b64a13bb93a070bf174b279d2d2c3c85dbf06
tanviredu <ornobtanvir.git@gmail.com>
2020-06-20 05:08:09 +0600

    added

--- 14_learning_django/wisdompets/app/admin.py
+++ 14_learning_django/wisdompets/app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Pet,Vaccine
 
-# Register your models here.
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display   = ('id','name','sex','age','submitter')
+    list_filter    = ('id','name','sex','age','submitter')
+    search_fields  = ('name','sex','age')
+    date_hierarchy = 'submission_date'
+
+@admin.register(Vaccine)
+class VaccineAdmin(admin.ModelAdmin):
+    list_display   = ('id','name')
+    list_filter    = ('id','name')
+    search_fields  = ('id','name')
+
+
+    
--- 14_learning_django/wisdompets/app/models.py
+++ 14_learning_django/wisdompets/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    #          a pet can have many vaccine 
+    # and a vaccine can given to many pets
+    SEX_CHOICE       = (('M','Male'),('F','Female'))
+    name             = models.CharField(max_length=100)
+    submitter        = models.CharField(max_length=100)
+    submission_date  = models.DateTimeField()
+    description      = models.TextField(blank=True)
+    sex              = models.CharField(max_length=2,choices=SEX_CHOICE)
+    age              = models.IntegerField(null=True)
+    vaccinations     = models.ManyToManyField('Vaccine',blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class Vaccine(models.Model):
+    # a pet can have many vaccine 
+    # and a vaccine can given to many pets
+    name = models.CharField(max_length=50)
+    
